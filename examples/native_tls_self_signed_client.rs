717b83c1463bf0e4a9dde2807f5708943da1991f
Jens Reidel <adrian@travitia.xyz>
2023-05-28 22:12:38 +0200

    Add native-tls self-signed cert client example
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ examples/native_tls_self_signed_client.rs
@@ -0,0 +1,25 @@
+use http::Uri;
+use tokio_native_tls::native_tls::{Certificate, TlsConnector};
+use tokio_websockets::{ClientBuilder, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let uri = Uri::from_static("wss://127.0.0.1:8080");
+    let bytes = std::fs::read("certs/localhost.crt")?;
+    let cert = Certificate::from_pem(&bytes)?;
+    let connector = TlsConnector::builder().add_root_certificate(cert).build()?;
+    let connector = tokio_websockets::Connector::NativeTls(connector.into());
+
+    let mut client = ClientBuilder::from_uri(uri)
+        .connector(&connector)
+        .connect()
+        .await?;
+
+    let msg = client.next().await;
+
+    println!("Got message: {msg:?}");
+
+    client.close(None, None).await?;
+
+    Ok(())
+}
--- examples/selfsigned-cert.sh
+++ examples/selfsigned-cert.sh
@@ -5,4 +5,4 @@ cd certs
 openssl req -x509 -out localhost.crt -keyout localhost.key \
   -newkey rsa:4096 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
-   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=IP:127.0.0.1\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
