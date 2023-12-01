0b8804af39e0391ba3b6a31c5ce98fa047fca587
Jens Reidel <adrian@travitia.xyz>
2023-12-01 06:59:53 +0100

    Remove timers in benchmark
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- examples/echo-server.rs
+++ examples/echo-server.rs
@@ -6,15 +6,14 @@ use tokio_websockets::{Config, Error, Limits, ServerBuilder};
 
 const PORT: u16 = 3000;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Error> {
+async fn run() -> Result<(), Error> {
     let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
     let listener = TcpListener::bind(addr).await?;
 
     loop {
         let (conn, _) = listener.accept().await?;
 
-        tokio::spawn(async move {
+        tokio::spawn(tokio::task::unconstrained(async move {
             let mut server = unsafe {
                 ServerBuilder::new()
                     .config(Config::default().frame_size(usize::MAX))
@@ -29,6 +28,17 @@ async fn main() -> Result<(), Error> {
                     unsafe { server.send(item).await.unwrap_unchecked() };
                 }
             }
-        });
+        }));
     }
+}
+
+fn main() -> Result<(), Error> {
+    let rt = unsafe {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .build()
+            .unwrap_unchecked()
+    };
+
+    rt.block_on(run())
 }
