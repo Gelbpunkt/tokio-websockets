a23624a2b02d4200f46ec9c77f60e073935672d9
Jens Reidel <adrian@travitia.xyz>
2023-12-29 20:09:32 +0100

    Rehaul examples a tiny bit
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- examples/client.rs
+++ examples/client.rs
@@ -1,11 +1,15 @@
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 use http::Uri;
-use tokio_websockets::{ClientBuilder, Error};
+use tokio_websockets::{ClientBuilder, Error, Message};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let uri = Uri::from_static("wss://gateway.discord.gg");
+    let uri = Uri::from_static("ws://127.0.0.1:3000");
     let (mut client, _) = ClientBuilder::from_uri(uri).connect().await?;
+
+    client
+        .send(Message::text(String::from("Hello, world!")))
+        .await?;
 
     while let Some(item) = client.next().await {
         println!("{item:?}");
