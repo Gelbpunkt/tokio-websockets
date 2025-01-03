c82eecc88bdb2caa68e66224f6db34a98e0fac63
Jens Reidel <adrian@travitia.xyz>
2025-01-03 18:36:55 +0100

    Fix docs
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- src/client.rs
+++ src/client.rs
@@ -242,9 +242,6 @@ impl<'a, R: Resolver> Builder<'a, R> {
     ///
     /// Returns [`Error::DisallowedHeader`] if the header is in
     /// the [`DISALLOWED_HEADERS`] list.
-    ///
-    /// [`Error::DisallowedHeader`]: Error::DisallowedHeader
-    /// [`DISALLOWED_HEADERS`]: Self::DISALLOWED_HEADERS
     pub fn add_header(mut self, name: HeaderName, value: HeaderValue) -> Result<Self, Error> {
         if DISALLOWED_HEADERS.contains(&name) {
             return Err(Error::DisallowedHeader);
